ca91dae7d96d39cd2abd776ddc2b0eb04f41f5c7
MG <migeorge@redhat.com>
2024-09-03 13:59:40 +1000

    init

---
+++ collections/ansible_collections/ansible/eda/tests/unit/event_filter/test_insert_hosts_to_meta.py
@@ -0,0 +1,82 @@
+import pytest
+
+from extensions.eda.plugins.event_filter.insert_hosts_to_meta import PathNotExistError
+from extensions.eda.plugins.event_filter.insert_hosts_to_meta import main as hosts_main
+
+EVENT_DATA_1 = [
+    (
+        {"app": {"target": "webserver"}},
+        {"host_path": "app.target"},
+        ["webserver"],
+    ),
+    (
+        {"app": {"target": "webserver;postgres"}},
+        {
+            "host_path": "app/target",
+            "path_separator": "/",
+            "host_separator": ";",
+        },
+        ["webserver", "postgres"],
+    ),
+    (
+        {
+            "app": {"target": ["webserver", "postgres"]},
+            "meta": {"source": "upstream"},
+        },
+        {
+            "host_path": "app.target",
+        },
+        ["webserver", "postgres"],
+    ),
+    (
+        {"app": "foo", "meta": {"source": "upstream"}},
+        {
+            "host_path": "bar",
+        },
+        [],
+    ),
+]
+
+
+@pytest.mark.parametrize("data, args, expected_hosts", EVENT_DATA_1)
+def test_find_hosts(data, args, expected_hosts):
+    data = hosts_main(data, **args)
+    if expected_hosts:
+        assert data["meta"]["hosts"] == expected_hosts
+    else:
+        assert "hosts" not in data["meta"]
+
+
+EVENT_DATA_2 = [
+    (
+        {"app": {"target": 5000}},
+        {"host_path": "app.target"},
+    ),
+    (
+        {"app": {"target": ("host1", 5000)}},
+        {"host_path": "app.target"},
+    ),
+    (
+        {"app": {"target": {"foo": "bar"}}},
+        {"host_path": "app.target"},
+    ),
+]
+
+
+@pytest.mark.parametrize("data, args", EVENT_DATA_2)
+def test_fail_find_hosts(data, args):
+    with pytest.raises(TypeError):
+        hosts_main(data, **args)
+
+
+def test_host_path_not_exist():
+    event = {"app": {"target": 5000}}
+    host_path = "app.bad"
+    assert hosts_main(event, host_path=host_path) == event
+
+
+def test_host_path_not_exist_exception():
+    event = {"app": {"target": 5000}}
+    host_path = "app.bad"
+    with pytest.raises(PathNotExistError):
+        hosts_main(event, host_path=host_path, raise_error=True)
